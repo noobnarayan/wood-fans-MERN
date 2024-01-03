354ce7afab693e20db0eebd70a68475a3f73f711
Narayan Das <workmail.narayan@gmail.com>
2024-01-03 22:45:42 +0530

    refactor: optimize error handeling

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -6,9 +6,21 @@ const { ApiResponse } = require('../utils/ApiResponse.js');
 const jwt = require('jsonwebtoken');
 
 const getAllProducts = asyncHandler(async (req, res) => {
-    const products = await Product.find()
-    res.status(201).json(new ApiResponse(200, products, "Get products successful"))
-})
+    try {
+        const products = await Product.find();
+        if (!products) {
+            throw new ApiError(404, "No products found");
+        }
+        res.status(200).json(new ApiResponse(200, products, "Get products successful"));
+    } catch (error) {
+        if (error instanceof ApiError) {
+            throw error;
+        } else {
+            throw new ApiError(500, "Internal Server Error");
+        }
+    }
+});
+
 
 
 module.exports = {
